03201a75fbfe2fb023bcfa2d0c68a95023cb6f44
marginalfriend <abdullahpsg@gmail.com>
2024-07-06 20:56:28 +0700

    [CHANGES] least important function at the bottom

--- api.go
+++ api.go
@@ -7,30 +7,7 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
-)
-
-func WriteJSON(w http.ResponseWriter, status int, v any) error {
-	w.WriteHeader(status)
-	w.Header().Set("Content-Type", "application/json")
-
-	return json.NewEncoder(w).Encode(v)
-}
-
-// Our API handler function signature
-type apiFunc func(http.ResponseWriter, *http.Request) error
-
-type APIError struct {
-	Error string
-}
-
-// Function to make our API handler functions compatible for HTTP handler functions
-func makeHTTPHandlerFunc(f apiFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		if err := f(w, r); err != nil {
-			WriteJSON(w, http.StatusBadRequest, APIError{Error: err.Error()})
-		}
-	}
-} 
+) 
 
 type APIServer struct {
 	listenAddr string
@@ -87,4 +64,27 @@ func (s *APIServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request) 
 
 func (s *APIServer) handleTransfer(w http.ResponseWriter, r *http.Request) error {
 	return nil
+}
+
+func WriteJSON(w http.ResponseWriter, status int, v any) error {
+	w.WriteHeader(status)
+	w.Header().Set("Content-Type", "application/json")
+
+	return json.NewEncoder(w).Encode(v)
+}
+
+// Our API handler function signature
+type apiFunc func(http.ResponseWriter, *http.Request) error
+
+type APIError struct {
+	Error string
+}
+
+// Function to make our API handler functions compatible for HTTP handler functions
+func makeHTTPHandlerFunc(f apiFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := f(w, r); err != nil {
+			WriteJSON(w, http.StatusBadRequest, APIError{Error: err.Error()})
+		}
+	}
 }
