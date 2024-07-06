9e414a20cda2e72fa2456a92b00bb3431b70ab52
marginalfriend <abdullahpsg@gmail.com>
2024-07-06 20:10:46 +0700

    [NEW] http handler signature function and error handling

--- api.go
+++ api.go
@@ -1,5 +1,35 @@
 package main
 
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func WriteJSON(w http.ResponseWriter, status int, v any) error {
+	w.WriteHeader(status)
+	w.Header().Set("Content-Type", "application/json")
+
+	return json.NewEncoder(w).Encode(v)
+}
+
+// Our API handler function signature
+type apiFunc func(http.ResponseWriter, *http.Request) error
+
+type APIError struct {
+	Error string
+}
+
+// Function to make our API handler functions compatible for HTTP handler functions
+func makeHTTPHandlerFunc(f apiFunc) http.HandleFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := f(w, r); err != nil {
+			WriteJSON(w, http.StatusBadRequest, APIError{Error: err.Error()})
+		}
+	}
+} 
+
 type APIServer struct {
 	listenAddr string
 }
@@ -10,6 +40,31 @@ func NewAPIServer(listenAddr string) *APIServer {
 	return &APIServer{
 		listenAddr: listenAddr,
 	}
-	
+
 }
 
+func (s *APIServer) Run() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/account", makeHTTPHandlerFunc(s.handleAccount))
+}
+
+func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func (s *APIServer) handleGetAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func (s *APIServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func (s *APIServer) handleTransfer(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
